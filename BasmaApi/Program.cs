13e32382c27bfe4c4a8ca3a49c01fc966d4e9ae8
kabtenkemo <akmlmhfwz@gmail.com>
2026-04-15 16:40:12 +0200

    vote fix

--- BasmaApi/Program.cs
+++ BasmaApi/Program.cs
@@ -15,7 +15,12 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
